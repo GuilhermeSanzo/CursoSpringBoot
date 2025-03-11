f7b0f8e8f7a743bae68ad829c136c49d8d0800c1
Guilherme Souza <81893267+GuilhermeSanzo@users.noreply.github.com>
2025-03-11 14:13:30 -0300

    persistindo produtos no banco de dados

--- produtosapi/src/main/java/io/github/guilhermesanzo/produtosapi/controller/ProdutoControler.java
+++ produtosapi/src/main/java/io/github/guilhermesanzo/produtosapi/controller/ProdutoControler.java
@@ -1,18 +1,32 @@
 package io.github.guilhermesanzo.produtosapi.controller;
 
 import io.github.guilhermesanzo.produtosapi.model.Produto;
+import io.github.guilhermesanzo.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("produtos")
 public class ProdutoControler {
 
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoControler(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
     @PostMapping
     public Produto salvar(@RequestBody Produto produto) {
         System.out.println("Produto recebido: " + produto);
+
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+
+        produtoRepository.save(produto);
         return produto;
     }
 
