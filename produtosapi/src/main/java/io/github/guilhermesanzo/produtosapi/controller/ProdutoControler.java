9a6d6ceb427e7dc13ce444742f47c3a49cd34a3e
Guilherme Souza <81893267+GuilhermeSanzo@users.noreply.github.com>
2025-03-14 23:03:26 -0300

    obtendo os dados do produto

--- produtosapi/src/main/java/io/github/guilhermesanzo/produtosapi/controller/ProdutoControler.java
+++ produtosapi/src/main/java/io/github/guilhermesanzo/produtosapi/controller/ProdutoControler.java
@@ -2,11 +2,9 @@ package io.github.guilhermesanzo.produtosapi.controller;
 
 import io.github.guilhermesanzo.produtosapi.model.Produto;
 import io.github.guilhermesanzo.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -28,6 +26,11 @@ public class ProdutoControler {
 
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto obterPorId(@PathVariable("id") String id) {
+        return produtoRepository.findById(id).orElse(null);
     }
 
 }
